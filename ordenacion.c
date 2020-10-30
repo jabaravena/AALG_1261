3e96f392405bc6a97d2acdf0eb37cc41a1a637e5
Jose A <joseubuntu@jose.notebookHP>
2020-10-30 19:42:46 +0100

    Primer esbozo en C de MergeSort y Merge

--- ordenacion.c
+++ ordenacion.c
@@ -91,3 +91,83 @@ int InsertSortInv(int* tabla, int ip, int iu)
 }
 
 
+
+/*CABECERA*/
+int mergesort(int* tabla, int ip, int iu) {
+  int med = 0;
+  int obs = 0;
+  int mer = 0;
+  /*No pongo el primer CdE del pscodigo pq lo diré en la cabecera*/
+  if(!tabla) {
+    return ERR;
+    /*Este CdE lo hago pq como vamos a llamar a más MergeSorts y estos llaman
+    a Merge/Combinar y esta rutina utiliza memoria dinamica, la cual puede 
+    fallar; pues hago este control de errores;
+    Esto es un poco lío, lo iré vienod mientras avance*/
+  }
+
+  if(ip == iu) {
+    return 0;
+  }
+
+  med = (iu - ip) / 2;
+  obs = mergesort(tabla, ip, med);
+  obs += mergesort(tabla, med + 1, iu);
+  mer = merge(tabla, ip, iu, med);
+
+  if(ERR == mer) {
+    /*algo que libere o yo que sé*/
+  }
+  else {
+    obs += mer;
+  }
+  return obs;
+}
+
+
+/*CABECERA*/
+int merge(int* tabla, int ip, int iu, int imedio) {
+  int * aux;
+  int i,j,k;
+  int obs=0;
+
+  aux = malloc(iu - ip +1);
+  if(!aux) {
+    return ERR;
+    /*Yo creo que tendré que devolver -1 pq ERR no es int, right??*/
+  }
+
+  i = ip;
+  j = imedio +1;
+  k = 0;
+
+  while(i <= imedio && j <= iu) {
+    if(tabla[i] < tabla[j]) {
+      aux[k] = tabla[i];
+      i++;
+    }
+    else {
+      aux[k] = tabla[j];
+      j++;
+    }
+    k++;
+    obs++;
+  }
+
+  while(j <= iu) {
+    aux[k] = tabla[j];
+    j++;
+    k++;
+  }
+
+  while(i <= imedio) {
+    aux[k] = tabla[i];
+    i++;
+    k++;
+  }
+
+  copy(tabla, aux, ip, iu);
+  free(aux);
+
+  return obs;
+}
